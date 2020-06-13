61f13600c278b04f2ffaef091166bf1006269396
Aiden Diederiks <u17306312@tuks.co.za>
2020-06-13 19:24:08 +0200

    Refactored Styling

--- js/script.js
+++ js/script.js
@@ -1,32 +1,45 @@
 const languages = [ 'Javascript', 'HTML & CSS', 'Java', 'C++', 'C#', 'PHP', 'ASP.NET', 'NodeJS' ];
-const langSkill = [ 86, 90, 80, 70, 80, 79, 85, 80 ];
+const langSkill = [ 86, 80, 67, 67, 80, 69, 85, 80 ];
+let skill_flag = false;
+
+
 
 $(document).ready(() => {
     console.log('loaded');
+
+    $('#reddit').on('click', () => {
+        console.log('reddit clicked');
+        reddit_redirect()
+    });
+
+    $('#linkedin').on('click', () => {
+        console.log('linkedin clicked');
+        linkedin_click();
+    });
+
+    $('#github').on('click', () => {
+        console.log('github clicked');
+        github_click();
+    });
 
     for (let index = 0; index < languages.length; index++) {
         $('.skills-box').append(
         `
             <div class="row">
-                <div class="skill-name ml-5 col-lg-4 mt-4 col-md-6 col-12">
-                    ${languages[index]} 
+                <div class="skill-name bar-description col-lg-4 mt-4 col-md-6 col-12">
+                    <span class="skill-text">${languages[index]}</span>
                 </div>
-                <div class="skill-level col-lg-1 col-md-2 col-3">
-                    <div class="count">
-                        ${langSkill[index]}
+                
+                <div class="skill-bar" style="width: ${langSkill[index]}%; height: 1vh;">
+                    <div class="bar">
+                        <span class="bar-info">${langSkill[index]}</span>
                     </div>
                 </div>
             </div>
         `);
-        if ((index+1) != languages.length) {
-            $('.skills-box').append(
-                `
-                <div class="divider">
-                </div>
-                `
-            );
-        }
     }
+
+    $('#button').click();
 
     $('.count').each(function () {
         $(this).prop('Counter',0).animate({
@@ -39,4 +52,45 @@ $(document).ready(() => {
             }
         });
     });
+
+
+    $('.skills-div').hover(function() {
+        console.log('hover');
+
+        if (!skill_flag) {
+            $(this).addClass('dark');
+            $(this).removeClass('light');
+            $('.bar').addClass('light');
+            $('.bar').removeClass('dark');
+            $('.skill-text').addClass('dark-text');
+            $('.skill-text').removeClass('light-text');
+            $('.bar-info').addClass('inverted-dark-text');
+            $('.bar-info').removeClass('inverted-light-text');
+        } else {
+            $(this).addClass('light');
+            $(this).removeClass('dark');
+            $('.bar').addClass('dark');
+            $('.bar').removeClass('light');
+            $('.skill-text').addClass('light-text');
+            $('.skill-text').removeClass('dark-text');
+            $('.bar-info').addClass('inverted-light-text');
+            $('.bar-info').removeClass('inverted-dark-text');
+        }
+
+        skill_flag = !skill_flag;
+    });
+    
 })
+
+
+function reddit_redirect() {
+    window.location.href = 'https://www.reddit.com/user/R4NG3R_za/';
+}
+
+function linkedin_click() {
+    window.location.href = 'https://www.linkedin.com/in/aiden-diederiks-3045801a5/';
+}
+
+function github_click () {
+    window.location.href = 'https://github.com/le-ginger';
+}
