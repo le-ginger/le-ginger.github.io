5c9af2adf4ab0b90ba13c227883b4d3140fc5531
Aiden Diederiks <u17306312@tuks.co.za>
2020-07-04 11:43:41 +0200

    mobile adjustments
    social icon positioning, added back to top button

--- js/script.js
+++ js/script.js
@@ -7,18 +7,10 @@ let skill_flag = false;
 $(document).ready(() => {
     console.log('loaded');
 
-
-
-
-    
-
-      
-
-
-
-
-
-
+    $('#to_top').click(() => {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
 
     $('#reddit').on('click', () => {
         console.log('reddit clicked');
